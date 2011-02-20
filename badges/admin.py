3c78195a5e68eef7e39b1f8c0086a97e8c0ba89e
Eher <alexandre@skd.com.br>
2011-02-20 19:39:15 -0300

    Importação do projeto.

---
+++ badges/admin.py
@@ -0,0 +1,6 @@
+from badges.models import User, Badge, Rule, History
+from django.contrib import admin
+
+admin.site.register(Badge)
+admin.site.register(Rule)
+admin.site.register(History)
---
+++ badges/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#class User(models.Model):
+#	name = models.CharField(max_length=30)
+#	provider = models.CharField(max_length=30)
+#	token = models.CharField(max_length=100)
+#
+#	def __unicode__(self):
+#		return self.name
+
+class Badge(models.Model):
+	name = models.CharField(max_length=30)
+	photoUrl = models.CharField(max_length=100)
+	description = models.CharField(max_length=400)
+
+	def __unicode__(self):
+		return self.name
+
+class Rule(models.Model):
+	owner = models.ForeignKey(User)
+	query = models.CharField(max_length=400)
+	minCount = models.IntegerField()
+	badge = models.ForeignKey(Badge)
+
+	def __unicode__(self):
+		return self.query
+
+class History(models.Model):
+	date = models.DateTimeField("Data/Hora")
+	user = models.ForeignKey(User)
+	badge = models.ForeignKey(Badge)
+
+	def __unicode__(self):
+		return self.query
